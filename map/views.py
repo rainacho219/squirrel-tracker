79f4e304448ff4417db0e12a3f5aed9d3f1ddc9d
Jenny Jihyuon Park <jp3256@columbia.edu>
2019-12-01 20:16:19 +0000

    Add map/views.py

--- map/views.py
+++ map/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from sightings.models import Sightings
+import random
+
+def map(request):
+    sample = random.sample(range(Sightings.objects.count()),100)
+    sightings = [Sightings.objects.all()[i] for i in sample]
+    context = {
+        'sightings': sightings,
+    }
+    return render(request, 'sightings/map.html', context)
 
 # Create your views here.
